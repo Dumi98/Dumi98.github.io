f0a29533d8c250daa5853cc62eca25ae0b309fb5
Dumi98 <56539761+Dumi98@users.noreply.github.com>
2019-11-04 14:53:13 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -1,6 +1,6 @@
 document.getElementById("id_logic").innerHTML = "logic = 2019.11.04.1"
-window.addEventListener("devicemotion", on_acc_data_uab);
 window.addEventListener("deviceorientation", on_gyro_data_uab);
+window.addEventListener("devicemotion", on_acc_data_uab);
 
 function on_gyro_data_uab(e)
 {
