6752e2a66337832e85064dc85dafab35855f4710
Dumi98 <56539761+Dumi98@users.noreply.github.com>
2019-11-04 14:50:14 +0200

    Add files via upload

--- gyro.js
+++ gyro.js
@@ -1,10 +1,18 @@
-document.getElementById("").innerHTML = "logic = 2019.11.04.0"
-
-window.adddEventListener("deviceorientation", on_gyro_data_uab);
+document.getElementById("id_logic").innerHTML = "logic = 2019.11.04.1"
+window.addEventListener("devicemotion", on_acc_data_uab);
+window.addEventListener("deviceorientation", on_gyro_data_uab);
 
 function on_gyro_data_uab(e)
 {
-	document.getElementById("id_alpha").innerHTML = e.alpha;
-	document.getElementById("id_beta").innerHTML = e.beta;
-	document.getElementById("id_gamma").innerHTML = e.gamma;
+	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha*100)/100;
+	document.getElementById("id_beta").innerHTML = Math.round(e.beta*100)/100;
+	document.getElementById("id_gamma").innerHTML = Math.rond(e.gamma*100)/100;
+}
+
+function on_acc_data_uab(e)
+{
+	var acc = e.accelerationIncludingGravity;
+	document.getElementById("id_acc_x").innerHTML = Math.round(e.acc.x*100)/100;
+	document.getElementById("id_acc_y").innerHTML = Math.round(e.acc.y*100)/100;
+	document.getElementById("id_acc_z").innerHTML = Math.round(e.acc.z*100)/100;
 }
