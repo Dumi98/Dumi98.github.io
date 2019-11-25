2b582080af802ce209426fac95dd883554ede533
Dumi98 <56539761+Dumi98@users.noreply.github.com>
2019-11-25 13:45:05 +0200

    Add files via upload

--- worker/prime.js
+++ worker/prime.js
@@ -1,4 +1,13 @@
 
+onmessage = function f(e){
+	if (e.data == "start"){
+		   ruleaza = true;
+	}
+	else 
+		if (e.data == "stop"){
+			ruleaza = false;
+		}
+}
 function este_prim(n)
 {
  for (var i = 2; i*i <= n;i++)
@@ -7,6 +16,20 @@ function este_prim(n)
  return true;	  
 }
 
-for (var i = 1E9; i < 1E11; i ++)
+function start()
+{
+	if (ruleaza == false)
+		;
+	else{
+for (var i = start_value; i < start_value+100; i ++)
 	if (este_prim(i))
 		postMessage(i);
+	start_value += 100;
+	}
+	if (start_value < 1E11)
+	setTimeout(start, 1, start_value);
+      else
+		  postMessage("gata");
+}
+
+start(1E10);
